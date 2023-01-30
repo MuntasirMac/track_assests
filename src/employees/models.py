0674244c1f904eb968f1ce145d3cb65850f15d0c
MuntasirMac <Muntasir120@gmail.com>
2023-01-30 19:24:28 +0600

    added gitignore and initialised employee model

--- src/employees/models.py
+++ src/employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from company.models import Company
+import uuid
 
-# Create your models here.
+class Employee(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    name = models.CharField(max_length=128)
+    designation = models.CharField(max_length=128)
+    phone = models.CharField(max_length=32)
+    nid = models.CharField(max_length=32, blank=True, null=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
